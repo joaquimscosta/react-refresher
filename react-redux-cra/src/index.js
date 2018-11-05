f1640d4f967d238e8633cfe37b1fbd5bf8fe189b
Joaquim Costa <joaquim.costa@ge.com>
2018-11-05 16:47:07 -0500

    scoreboard

---
+++ react-redux-cra/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PlayerReducer from './reducers/player';
+import Scoreboard from './containers/Scoreboard';
+import './App.css';
+
+const store = createStore(
+  PlayerReducer,
+  window.devToolsExtension && window.devToolsExtension()
+);
+
+render(
+  <Provider store={store}>
+    <Scoreboard />
+  </Provider>,
+  document.getElementById('root')
+);
+
