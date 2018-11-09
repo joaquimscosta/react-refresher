b9a482489cf1e3b81377e3e16f34e3af81c0630f
Joaquim Costa <jscosta88@gmail.com>
2018-11-09 09:37:46 -0500

    remove java code

--- build_rest_api_in_spring/src/main/java/com/example/coursereview/review/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.coursereview.review;
-
-import org.springframework.data.repository.CrudRepository;
-/**
- * ReviewRepository
- */
-public interface ReviewRepository extends CrudRepository<Review,Long> {}
