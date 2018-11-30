985709d68fa5ad291f75bd7b4ca40626c2095db7
Joaquim Costa <joaquim.costa@ge.com>
2018-11-29 20:03:35 -0500

    chapter 4 lifecycle and render

---
+++ react-lifecycle/src/components/ChildComponent.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+class ChildComponent extends Component{
+    static propTypes = {
+        name: PropTypes.string
+    }
+    static defaultProps = (function(){
+        console.log('ChildComponent: defaultProps')
+        return {}
+    })()
+
+    constructor(props){
+        super(props)
+        console.log('ChildComponent: state')
+        this.state = {
+            name: 'Joaquim'
+        }
+    }
+    // Any changes within 'state' here wont trigger rerender
+    componentWillMount(){
+        console.log('ChildComponent: componentWillMount')
+    }
+    // good place to update state with data coming from network request
+    // will trigger rerender
+    componentDidMount(){
+        console.log('ChildComponent: componentDidMount')
+    }
+
+    componentWillReceiveProps(nextProps){
+        console.log('ChildComponent: componentWillReceiveProps')
+        console.log('nextProps:', nextProps)
+    }
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('<ChildComponent/>: shouldComponentUpdate')
+        console.log('nextProps:', nextProps)
+        console.log('nextState:', nextState)
+        return true
+    }
+    componentWillUpdate(previousProps, previousState){
+        console.log('<ChildComponent/>: componentWillUpdate')
+        console.log('previousProps:', previousProps)
+        console.log('previousState:', previousState)
+        return true
+    }
+    componentDidUpdate(previousProps, previousState){
+        console.log('<ChildComponent/>: componentDidUpdate')
+        console.log('previousProps:', previousProps)
+        console.log('previousState:', previousState)
+        return true
+    }
+
+    render(){
+        console.log('ChildComponent: render')
+        if(this.state.oops){
+            throw new Error('Something went wrong')
+        }
+        return [
+            <div key='name'> Name: {this.props.name} </div>
+        ]
+    }
+}
+export default ChildComponent
---
+++ react-lifecycle/src/components/ParentComponent.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+import ChildComponent from './ChildComponent'
+
+class ParentComponent extends Component{
+    static defaultProps = (function(){
+        console.log('ParentComponent: defaultProps')
+        return {
+            true: false
+        }
+    })()
+    componentWillMount(){
+        console.log('ParentComponent: componentWillMount')
+    }
+    componentDidMount(){
+        console.log('ParentComponent: componentDidMount')
+    }
+
+    constructor(props){
+        super(props)
+        this.state = {
+            text: ''
+        }
+        this.onInputChange = this.onInputChange.bind(this)
+    }
+    onInputChange(event){
+        const text = event.target.value
+        this.setState(()=>({
+            text:text
+        }))
+    }
+    render(){
+        console.log('ParentComponent: render')
+        return [
+                <h2 key='h2'>Learn about rendering and lifecycle methods!</h2>,
+                <input key='input' value={this.state.text} onChange={this.onInputChange}/>,
+                <ChildComponent key='ChildComponent' name={this.state.text}/>
+        ]
+    }
+}
+export default ParentComponent
