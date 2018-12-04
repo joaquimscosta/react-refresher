988384d316855cc2c2a4d14cd8aa718f3a01be03
Joaquim Costa <joaquim.costa@ge.com>
2018-12-04 16:37:09 -0500

    components in react

---
+++ comment/src/CommentDetail.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CommentDetail = (props) => {
+  return (
+    <div className='comment'>
+      <a href='/' className='avatar'>
+        <img alt='avatar' src={props.avatar} />
+      </a>
+      <div className='content'>
+        <a href='/' className='author'>{props.author}</a>
+      </div>
+      <div className='metadata'>
+        <span>{props.time}</span>
+        <div className='text'>{props.text}</div>
+      </div>
+    </div>
+  )
+}
+
+CommentDetail.propTypes = {
+  text: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  timeAgo: PropTypes.string.isRequired
+}
+
+export default CommentDetail
---
+++ comment/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import faker from 'faker'
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
+
+const App = () => {
+  return (
+    <div className='ui container comments'>
+      <ApprovalCard>
+        <CommentDetail author='Sam' avatar={faker.image.avatar()} timeAgo='Today at 6:00PM' text='nice blog post' />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author='Alex' avatar={faker.image.avatar()} timeAgo='Today at 8:00PM' text='I love React' />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author='Jane' avatar={faker.image.avatar()} timeAgo='Yesterday at 10:00AM' text='Can you explain more about Redux' />
+      </ApprovalCard>
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
