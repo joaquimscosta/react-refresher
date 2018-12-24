599ca14ec59675210ed11544c1a4fccaf5bff7dc
Joaquim Costa <jscosta88@gmail.com>
2018-12-24 17:12:58 -0500

    use hooks

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,47 +1,33 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import DisplaySeason from './SeasonDisplay'
-import Spinner from './Spinner'
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import DisplaySeason from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 const options = {
   enableHighAccuracy: true,
   timeout: 10000,
   maximumAge: 0
-}
+};
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      latitude: null,
-      errorMessage: ''
-    }
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      pos => setLat(pos.coords.latitude),
+      err => setErrorMessage(err.message)
+    );
+  }, []);
+
+  let content;
+  if (!errorMessage && lat) {
+    content = <DisplaySeason latitude={lat} />;
+  } else if (errorMessage && !lat) {
+    content = <div>Error: {errorMessage}</div>;
+  } else {
+    content = <Spinner message="Please accept location request" />;
   }
+  return content;
+};
 
-  componentDidMount () {
-    console.log('state=', this.state)
-    window.navigator.geolocation.getCurrentPosition((pos) => {
-      this.setState(() => ({
-        latitude: pos.coords.latitude
-      }))
-    }, (err) => {
-      console.error('error: ', err)
-      this.setState(() => ({
-        errorMessage: err.message
-      }))
-    }, options)
-  }
-
-  render () {
-    console.log('rendering.... state=', this.state)
-    if (!this.state.errorMessage && this.state.latitude) {
-      return <DisplaySeason latitude={this.state.latitude} />
-    }
-    if (this.state.errorMessage && !this.state.latitude) {
-      return <div>Error: {this.state.errorMessage}</div>
-    }
-    return <Spinner message='Please accept location request' />
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
