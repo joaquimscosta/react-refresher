71b144198d756e355e3e1c68482f1c5f11b49ab3
Joaquim Costa <jscosta88@gmail.com>
2018-12-25 16:39:41 -0500

    move app into archive

--- app2/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    { title: 'Javascript: the Good Parts', pages: 101 },
-    { title: 'Harry Potter', pages: 30 },
-    { title: 'The Dark Tower', pages: 21 },
-    { title: 'Eloquent Ruby', pages: 1 }
-  ]
-}
--- app4/src/components/posts_index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-
-class PostsIndex extends Component {
-  render() {
-    return <div>PostsIndex</div>
-  }
-}
-export default PostsIndex
