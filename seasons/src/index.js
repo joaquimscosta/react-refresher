c504e870ce0707770545a869af8c903d0d99894b
Joaquim Costa <jscosta88@gmail.com>
2018-12-25 16:37:31 -0500

    done for now

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,33 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import DisplaySeason from './SeasonDisplay'
 import Spinner from './Spinner'
-
-const options = {
-  enableHighAccuracy: true,
-  timeout: 10000,
-  maximumAge: 0
-}
+import useLocation from './useLocation'
 
 const App = () => {
-  const [lat, setLat] = useState(null)
-  const [errorMessage, setErrorMessage] = useState('')
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(
-      pos => setLat(pos.coords.latitude),
-      err => setErrorMessage(err.message)
-    )
-  }, [])
-
+  const [lat, errorMessage] = useLocation()
   let content
-  if (!errorMessage && lat) {
+  if (lat) {
     content = <DisplaySeason latitude={lat} />
-  } else if (errorMessage && !lat) {
+  } else if (errorMessage) {
     content = <div>Error: {errorMessage}</div>
   } else {
     content = <Spinner message='Please accept location request' />
   }
   return content
 }
-
 ReactDOM.render(<App />, document.getElementById('root'))
